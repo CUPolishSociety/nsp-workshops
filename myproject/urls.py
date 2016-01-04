39ed1b941e0d4595d7a8c4e0bd6b28b0c67155f3
Tamas Sztanka-Toth <sztankatt@gmail.com>
2016-01-04 15:05:37 +0100

    added new features

--- myproject/urls.py
+++ myproject/urls.py
@@ -4,15 +4,15 @@ from django.conf.urls.static import static
 
 import settings
 
-urlpatterns = patterns('',
+urlpatterns = [
     # Examples:
     # url(r'^$', 'myproject.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    #url(r'^$', 'workshops.views.workshop', name='workshop'),
-    #url(r'^CX6AD3QrLgcFZu85GZmLMrY1PoKDU9S4pqlJHAnPhtU5CFpMur/', 'workshops.views.responses'),
-#    url(r'^CX6AD3QrLgcFZu85GZmLMrY1PoKDU9S4pqlJHAnPhtU5CFpMur/zip/', 'workshops.views.return_zipped_file'),
-)
+    url(r'^$', 'workshops.views.workshop', name='workshop'),
+    url(r'^CX6AD3QrLgcFZu85GZmLMrY1PoKDU9S4pqlJHAnPhtU5CFpMur/', 'workshops.views.responses'),
+    url(r'^CX6AD3QrLgcFZu85GZmLMrY1PoKDU9S4pqlJHAnPhtU5CFpMur/zip/', 'workshops.views.return_zipped_file'),
+]
 
-#urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-#urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
--- workshops/migrations/0001_initial.py
+++ workshops/migrations/0001_initial.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import workshops.models
 
 
 class Migration(migrations.Migration):
@@ -18,8 +19,11 @@ class Migration(migrations.Migration):
                 ('surname', models.CharField(max_length=250)),
                 ('email', models.EmailField(unique=True, max_length=75)),
                 ('uni', models.CharField(max_length=200)),
-                ('graduation', models.IntegerField()),
-                ('cv', models.FileField(upload_to=b'cv')),
+                ('graduation', models.IntegerField(choices=[(1980, 1980), (1981, 1981), (1982, 1982), (1983, 1983), (1984, 1984), (1985, 1985), (1986, 1986), (1987, 1987), (1988, 1988), (1989, 1989), (1990, 1990), (1991, 1991), (1992, 1992), (1993, 1993), (1994, 1994), (1995, 1995), (1996, 1996), (1997, 1997), (1998, 1998), (1999, 1999), (2000, 2000), (2001, 2001), (2002, 2002), (2003, 2003), (2004, 2004), (2005, 2005), (2006, 2006), (2007, 2007), (2008, 2008), (2009, 2009), (2010, 2010), (2011, 2011), (2012, 2012), (2013, 2013), (2014, 2014), (2015, 2015), (2016, 2016), (2017, 2017), (2018, 2018), (2019, 2019), (2020, 2020)])),
+                ('w_1', models.CharField(max_length=30, choices=[(b'bain', b'Bain&Company'), (b'bzwbk', b'Bank Zachodni WBK'), (b'pwc', b'PwC'), (b'uber', b'Uber'), (b'martap', b'Marta Poslad')])),
+                ('w_2', models.CharField(max_length=30, choices=[(b'bain', b'Bain&Company'), (b'bzwbk', b'Bank Zachodni WBK'), (b'pwc', b'PwC'), (b'uber', b'Uber'), (b'martap', b'Marta Poslad')])),
+                ('w_3', models.CharField(blank=True, max_length=30, choices=[(b'bain', b'Bain&Company'), (b'bzwbk', b'Bank Zachodni WBK'), (b'pwc', b'PwC'), (b'uber', b'Uber'), (b'martap', b'Marta Poslad')])),
+                ('cv', models.FileField(upload_to=workshops.models.file_name, blank=True)),
             ],
             options={
             },
